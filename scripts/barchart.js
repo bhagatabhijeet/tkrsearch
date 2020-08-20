c682bf95c3949016a5edbf8ff2aedf8cde0fd584
bosshogg86 <jeffhogg86@gmail.com>
2020-08-19 23:23:05 -0700

    creates functions to call stockData and company profile

--- scripts/barchart.js
+++ scripts/barchart.js
@@ -1,11 +1,11 @@
 
 // Temporary value (need input from searchbox)
 let stockSymbols = 'AAPL'
+const stockApiKey = '93422ad3ea073e8cede10d31527869b6'
 
-// Stock API call
-function stockApi(stockSymbols){
+// Stock Data API call
+function stockApi(stockSymbols, stockApiKey){
     const stockURL = 'https://ondemand.websol.barchart.com/getQuote.json?apikey=${stockApiKey}&symbols=${stockSymbols}&fields=fiftyTwoWkHigh%2CfiftyTwoWkHighDate%2CfiftyTwoWkLow%2CfiftyTwoWkLowDate'
-    const stockApiKey = '93422ad3ea073e8cede10d31527869b6'
     $.ajax({
         url: stockURL,
         method: "GET",
@@ -13,3 +13,14 @@ function stockApi(stockSymbols){
         console.log(stockData);
     });
 }    
+
+// Get company profile
+function getProfile(stockSymbols, stockApiKey){
+    const profileURL = 'https://ondemand.websol.barchart.com/getProfile.json?apikey=${stockApiKey}&symbols=${stockSymbols}&fields=qtrOneEarnings%2CqtrTwoEarnings%2CqtrThreeEarnings%2CqtrFourEarnings'
+    $.ajax({
+        url: profileURL,
+        method: "GET",
+    }).then(function(profileData) {
+        console.log(profileData);
+    });
+}
