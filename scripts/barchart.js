f28a30c482301a5b8b71a26908c4fd12b097e0de
bosshogg86 <jeffhogg86@gmail.com>
2020-08-19 22:45:31 -0700

    creates stock api call function

--- scripts/barchart.js
+++ scripts/barchart.js
@@ -1,1 +1,15 @@
-// First comment
+
+// Temporary value (need input from searchbox)
+let stockSymbols = 'AAPL'
+
+// Stock API call
+function stockApi(stockSymbols){
+    const stockURL = 'https://ondemand.websol.barchart.com/getQuote.json?apikey=${stockApiKey}&symbols=${stockSymbols}&fields=fiftyTwoWkHigh%2CfiftyTwoWkHighDate%2CfiftyTwoWkLow%2CfiftyTwoWkLowDate'
+    const stockApiKey = '93422ad3ea073e8cede10d31527869b6'
+    $.ajax({
+        url: stockURL,
+        method: "GET",
+    }).then(function(stockData) {
+        console.log(stockData);
+    });
+}    
