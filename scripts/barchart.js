47b3ce45023dde24a9bd7b90881ebebf93369c63
Abhijeet Bhagat <abhagat@blackberry.com>
2020-08-19 23:36:43 -0700

    merge

--- scripts/barchart.js
+++ scripts/barchart.js
@@ -3,14 +3,9 @@
 let stockSymbols = 'AAPL'
 const stockApiKey = '93422ad3ea073e8cede10d31527869b6'
 
-<<<<<<< HEAD
-// Stock Data API call
-function stockApi(stockSymbols, stockApiKey){
-=======
 // Stock API call
 function stockApi(stockSymbols){
     const stockApiKey = '93422ad3ea073e8cede10d31527869b6'
->>>>>>> fa94c363a0635c9299ed564c66342472d58839a4
     const stockURL = 'https://ondemand.websol.barchart.com/getQuote.json?apikey=${stockApiKey}&symbols=${stockSymbols}&fields=fiftyTwoWkHigh%2CfiftyTwoWkHighDate%2CfiftyTwoWkLow%2CfiftyTwoWkLowDate'
     $.ajax({
         url: stockURL,
@@ -20,21 +15,8 @@ function stockApi(stockSymbols){
     });
 }    
 
-<<<<<<< HEAD
-// Get company profile
-function getProfile(stockSymbols, stockApiKey){
-    const profileURL = 'https://ondemand.websol.barchart.com/getProfile.json?apikey=${stockApiKey}&symbols=${stockSymbols}&fields=qtrOneEarnings%2CqtrTwoEarnings%2CqtrThreeEarnings%2CqtrFourEarnings'
-    $.ajax({
-        url: profileURL,
-        method: "GET",
-    }).then(function(profileData) {
-        console.log(profileData);
-    });
-}
-=======
 
 
 
 
 
->>>>>>> fa94c363a0635c9299ed564c66342472d58839a4
