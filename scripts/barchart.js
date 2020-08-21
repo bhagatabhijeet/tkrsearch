b134eb7ee4fe05178350943e073ccb1259f3277e
bosshogg86 <jeffhogg86@gmail.com>
2020-08-20 23:20:11 -0700

    add top 10

--- scripts/barchart.js
+++ scripts/barchart.js
@@ -5,7 +5,7 @@
     // Stock API call
     function getStock(stockSymbols){
         const stockApiKey = 'pk_9eb49acc515249ba85d431d6a16d502b'
-        const stockURL = `https://cloud.iexapis.com/v1/stock/${stockSymbols}/batch?types=quote,news,chart&token=${stockApiKey}`
+        const stockURL = `https://cloud.iexapis.com/v1/stock/${stockSymbols}/book?token=${stockApiKey}`
         $.ajax({
             url: stockURL,
             method: "GET",
@@ -14,6 +14,7 @@
         });
     }    
 
+    // Gets company data 
     function getCompany(stockSymbols){
         const stockApiKey = 'pk_9eb49acc515249ba85d431d6a16d502b'
         const companyURL = `https://cloud.iexapis.com/v1/stock/${stockSymbols}/company?token=${stockApiKey}`
@@ -22,5 +23,17 @@
             method: "GET",
         }).then(function(companyData) {
             console.log(companyData);
+        });
+    }
+
+    // Gets top 10 active stocks
+    function getTopActive(){
+        const stockApiKey = 'pk_9eb49acc515249ba85d431d6a16d502b'
+        const topURL = `https://cloud.iexapis.com/v1/stock/market/list/mostactive?token=${stockApiKey}`
+        $.ajax({
+            url: topURL,
+            method: "GET",
+        }).then(function(topData) {
+            console.log(topData);
         });
     }
