b8cd9d68c08ed6ec711457503f94c7c5749b03e9
Muhammad Kamran <mkamran1967@gmail.com>
2020-08-22 14:03:48 -0700

    unpause

--- script.js
+++ script.js
@@ -1,60 +1,75 @@
 // Import functions
-import { getTopCrypto } from './scripts/marketcoin.js';
+import { getTopCrypto, getCryptoBySymbol } from './scripts/marketcoin.js';
 import { getStock, getCompany, getTopStocks } from './scripts/stock.js';
 
-
-let stockSymbols = 'AAPL';
-// getStock('AAPL');
-
 $('document').ready(async () => {
-   // Populate Top Stocks
-   let topStocks = await getTopStocks();
-   console.log(topStocks);
-   let stockList = '';
-   let i = 1;
- 
-   // This will loop through the returned Data Array
-   topStocks.forEach((element) => {
-     // Creating a string of the following
-     stockList += ` 
-       <tr>
-           <th scope="row">${i++}</th>
-           <td>${element.companyName}</td>
-           <td>${element.symbol}</td>
-           <td>${element.latestPrice}</td>
-           <td>${element.marketCap}</td>
-       </tr>
-       `;
-   });
- 
-   // Append list into table body
-   $('#stockList').html(stockList);
- // }
-  // Get Top 10 Crypto from API
+  // // Populate Top Stocks
+  // let topStocks = await getTopStocks();
+  // console.log(topStocks);
+  // let stockList = '';
+  // let i = 1;
 
-  let topCrypto = await getTopCrypto();
+  // // This will loop through the returned Data Array
+  // topStocks.forEach((element) => {
+  //   // Creating a string of the following
+  //   stockList += `
+  //      <tr>
+  //          <th scope="row">${i++}</th>
+  //          <td>${element.companyName}</td>
+  //          <td>${element.symbol}</td>
+  //          <td>${element.latestPrice}</td>
+  //          <td>${element.marketCap}</td>
+  //      </tr>
+  //      `;
+  // });
 
-  // No Error in API Response -> Continue
-  if (topCrypto.error === false) {
-    let cryptoList = '';
+  // // Append list into table body
+  // $('#stockList').html(stockList);
+  // }
+  // // Get Top 10 Crypto from API
+  // let topCrypto = await getTopCrypto();
+  // // No Error in API Response -> Continue
+  // if (topCrypto.error === false) {
+  //   let cryptoList = '';
 
-    // This will loop through Data Array
-    topCrypto.Data.forEach((element, index) => {
-      // Concatening an HTML String of the following
-      cryptoList += `
-        <tr>
-          <th scope="row">${index + 1}</th>
-          <td>${element.CoinInfo.FullName}</td>
-          <td>${element.CoinInfo.Name}</td>
-          <td>${element.DISPLAY.USD.PRICE}</td>
-        </tr>
+  //   // This will loop through Data Array
+  //   topCrypto.Data.forEach((element, index) => {
+  //     // Concatening an HTML String of the following
+  //     cryptoList += `
+  //       <tr>
+  //         <th scope="row">${index + 1}</th>
+  //         <td>${element.CoinInfo.FullName}</td>
+  //         <td>${element.CoinInfo.Name}</td>
+  //         <td>${element.DISPLAY.USD.PRICE}</td>
+  //       </tr>
+  //     `;
 
-      `;
+  //     // Append list into table body
+  //     $('#cryptoList').html(cryptoList);
+  //   });
+  // }
 
-      // Append list into table body
-      $('#cryptoList').html(cryptoList);
-    });
-  }
+  $('#searchBtn').on('click', async (e) => {
+    e.preventDefault();
+    let response = {};
 
-  // Get Crypto if no Crypto check for Stock.
-});
+    if ($('#defaultInline1').prop('checked')) {
+      console.log(`Stocks`);
+    } else if ($('#defaultInline2').prop('checked')) {
+      let cSymbol = $('#searchInput').val().toUpperCase();
+      response = await getCryptoBySymbol(cSymbol);
+      if(response.HasWarning){
+        console.log(response.);
+      }
+
+      // Deconstruct
+      let { PRICE: price } = response.DISPLAY.cSymbol.USD;
+    } else {
+      console.log(`huh`);
+    }
+  });
+}); // End of Doc.ready()
+
+// function displayCrypto() {}
+// function getCrytpo() {}
+// function displayTopCrypto() {}
